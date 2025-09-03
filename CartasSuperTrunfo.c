307380113c7fdef21ca7c71aa8a236b213d0546f
LarissaFerreira01 <larissacostaferreira2013@gmail.com>
2025-09-03 03:39:11 +0000

    Finalização da atividade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,5 +25,83 @@ int main() {
     float pib2;
     int pontosTuristicos2;
 
+    // Dados para a Carta 1
+    printf("--- Cadastro da Carta 1 ---\n");
+    printf("Estado (A-H): ");
+    // O espaco antes de %c e importante para ignorar qualquer espaco em branco pendente.
+    scanf(" %c", &estado1); 
+
+    printf("Codigo da Carta (ex: A01): ");
+    scanf("%s", codigoCarta1);
+
+    printf("Nome da Cidade: ");
+    // Lê a linha inteira, incluindo espacos, ate a quebra de linha ('\n').
+    scanf(" %49[^\n]", nomeCidade1);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Area (em km2): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos1);
+    
+    printf("\n"); // Linha em branco para separar
+
+    // Dados para a Carta 2
+    printf("\n--- Cadastro da Carta 2 ---\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Codigo da Carta (ex: B02): ");
+    scanf("%s", codigoCarta2);
+
+    printf("Nome da Cidade: ");
+    scanf(" %49[^\n]", nomeCidade2);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("Area (em km2): ");
+    scanf("%f", &area2);
+
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    // Mensagem de Exibição dos Dados
+    printf("\n\n==================================\n");
+    printf("--- DADOS DAS CARTAS CADASTRADAS ---\n");
+    printf("==================================\n\n");
+
+    // Dados da Carta 1
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigoCarta1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km2\n", area1); // %.2f formata para duas casas decimais
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+
+    printf("\n"); // Linha em branco para separar 
+
+    // Dados da Carta 2
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigoCarta2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km2\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+
+    //Professor, não entendi o que seria esse "Estado" então presuponho que seja o Rank da carta" 
     return 0;
 }
